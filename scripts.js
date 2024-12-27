ee299d7e07e5cc7cd597d5c6eb968ad420f515d0
lwki-sy <187883197+lwki-sy@users.noreply.github.com>
2024-12-27 20:41:10 +0800

    Add a simple scoring system to score on a round

--- scripts.js
+++ scripts.js
@@ -43,12 +43,16 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
     if (humanChoice === computerChoice){
-        console.log("It's a tie!")
+        console.log("It's a tie!");
     } else if (humanChoice === "Rock" && computerChoice === "Paper" || humanChoice === "Paper" && computerChoice === "Scissors" || humanChoice === "Scissors" && computerChoice === "Rock"){
-        console.log("You lose.")
+        console.log("You lose.");
+        computerScore++;
     } else {
-        console.log("You win!")
+        console.log("You win!");
+        humanScore++;
     }
 }
 
 playRound(humanChoice, computerChoice);
+console.log("Your score: " + humanScore);
+console.log("Computer score: " + computerScore);
